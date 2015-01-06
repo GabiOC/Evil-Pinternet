1dab0deac43c08a993d711403c00ceb9dfb1a207
Gabriele O'Connor <oconnor.gabriele@gmail.com>
2015-01-05 20:04:26 -0500

    Added partials for signup/login modals

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207022509) do
+ActiveRecord::Schema.define(version: 20150106002949) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
